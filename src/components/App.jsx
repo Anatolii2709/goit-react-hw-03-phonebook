4dfe0bb31cd63c32e6c6bed4759332483c3fb2d9
Anatolii Tokariev <anatoliytockarev2709@gmail.com>
2023-08-03 21:26:17 +0200

    first

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,104 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { Container } from './App.styled';
+import ContactForm from './ContactForm/ContactForm';
+import { nanoid } from 'nanoid';
+
+import ContactList from './ContactList/ContactList';
+import Filter from './ Filter/Filter';
+
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  handleInputChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+    this.checkName(e);
+  };
+
+  checkName = e => {
+    const { contacts } = this.state;
+    return contacts.some(
+      contact => contact.name.toLowerCase() === e.target.value
+    );
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    let { contacts, name } = this.state;
+    if (
+      contacts.some(
+        contact => contact.name.toLowerCase() === name.toLowerCase()
+      )
+    ) {
+      toast.error(`${this.state.name} is already in contact !`, {
+        position: toast.POSITION.TOP_CENTER,
+      });
+      return;
+    }
+    let contact = {
+      id: nanoid(),
+      name: this.state.name,
+      number: this.state.number,
+    };
+
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, contact],
+    }));
+
+    toast.success(`You add contact ${this.state.name} to your phonebook.`, {
+      position: toast.POSITION.TOP_CENTER,
+    });
+
+    e.target.reset();
+  };
+
+  contactDelete = id => {
+    this.setState(prevState => {
+      return {
+        contacts: prevState.contacts.filter(contact => contact.id !== id),
+      };
+    });
+    toast.warn(`You deleted contact ${this.state.name} from your phonebook.`, {
+      position: toast.POSITION.TOP_CENTER,
+    });
+  };
+
+  filteredContacts = () => {
+    const { contacts, filter } = this.state;
+    return contacts
+      .sort((firstContact, secondContact) =>
+        firstContact.name.localeCompare(secondContact.name)
+      )
+      .filter(contact =>
+        contact.name.toLowerCase().includes(filter.toLowerCase())
+      );
+  };
+
+  render() {
+    return (
+      <Container>
+        <h1>Phonebook</h1>
+        <ContactForm
+          handleSubmit={this.handleSubmit}
+          handleInputChange={this.handleInputChange}
+        />
+        <h2>Contacts</h2>
+        <Filter handleInputChange={this.handleInputChange} />
+        <ContactList
+          contacts={this.filteredContacts()}
+          contactDelete={this.contactDelete}
+        />
+        <ToastContainer />
+      </Container>
+    );
+  }
+}
+
+export default App;
